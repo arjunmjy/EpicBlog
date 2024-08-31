9feb54a12766ecfc1794c35115c71ac645414283
Arjun Jyothilal <arjunmjtdyr@gmail.com>
2024-09-01 03:01:12 +0530

    updated header comp

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import OAuth from '../components/OAuth';
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
@@ -43,9 +44,9 @@ export default function SignUp() {
         <div className='flex-1'>
           <Link to='/' className='font-bold dark:text-white text-4xl'>
             <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-              Blog
+              EPIC
             </span>
-            Wave
+            Blog
           </Link>
           <p className='text-sm mt-5'>
             This is a demo project. You can sign up with your email and password
@@ -97,6 +98,7 @@ export default function SignUp() {
                 'Sign Up'
               )}
             </Button>
+            <OAuth />
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>Have an account?</span>
