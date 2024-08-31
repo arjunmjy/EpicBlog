14cfb073524c0a841556e1c1ff60fa890724a0f9
Arjun Jyothilal <arjunmjtdyr@gmail.com>
2024-08-31 19:18:16 +0530

    Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import Header from './components/Header'
+import Projects from './pages/Projects'
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/projects"element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
