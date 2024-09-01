ff677c7da7a42f5ab4386f4230a623e9e8fb22f5
Arjun Jyothilal <arjunmjtdyr@gmail.com>
2024-09-01 11:25:46 +0530

    show user posts inside dashboard

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,12 @@
-const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [flowbite.plugin(),],
-}
+  plugins: [require('flowbite/plugin'), require('tailwind-scrollbar')],
+};
